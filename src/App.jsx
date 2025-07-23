a14d25ab2152a99f180acfc8b6e00a0ac70bbd29
Falak sher <thisisfalaksher@gmail.com>
2025-07-23 15:19:26 +0500

    Create all routes and save in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/about'
+import Profile from './pages/profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <div><h1 className='text-3xl font-bold'>hello world</h1></div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-In' element={<SignIn/>} />
+        <Route path='/sign-Up' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
