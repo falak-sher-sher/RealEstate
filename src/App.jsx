2784fb1ee32bb361b93a118618ec1623edc3bb93
Falak sher <thisisfalaksher@gmail.com>
2025-07-24 09:49:52 +0500

    now i form header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/about'
 import Profile from './pages/profile'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/sign-In' element={<SignIn/>} />
