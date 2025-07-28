3dc89b2623b51b676ab69d46b199edb77395bdd6
Falak sher <thisisfalaksher@gmail.com>
2025-07-28 16:41:46 +0500

    Real

--- RealeEsate/src/pages/About.jsx
+++ RealeEsate/src/pages/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>about</div>
+    <div className='py-20 px-4 max-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>About Falak Estate</h1>
+      <p className='mb-4 text-slate-700'>Falak Estate is a real estate website that allows users to find, view, and contact property listings. It provides a platform for both property owners and potential buyers to connect and negotiate property prices.</p>
+
+      <p className='mb-4 txs'>The website is built using React, a JavaScript library for building user interfaces. It uses a combination of HTML, CSS, and JavaScript to create a responsive and interactive user interface.</p>
+      <p className='mb-4 txs'>The website is hosted on Netlify, a cloud-based platform that provides a fast and secure way to deploy and manage web applications.</p>
+    </div>
   )
 }
